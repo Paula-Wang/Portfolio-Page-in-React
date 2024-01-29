d8fc7175d429f719985eddcadc83edb03b134c6b
Paula-Wang <nimopaula8@gmail.com>
2024-01-29 15:16:35 +0300

    feat: :sparkles: add name and description about me

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,3 +1,22 @@
 export default function HeroSection() {
-    return <h1>HeroSection</h1>;
+    return (
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title">Hey, I am Pauline Wang'ombe</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color">Frontend</span>{""}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                    I am currently awaiting graduating with a Bachelor of Business and Information Technology. I am interested in the field of cybersecurity, IoT, Cloud and have dabbled in UI/UX. 
+                    <br /> I think VR/AR is a very underrated upcoming technology. It has many great uses and should be tapped into more.
+                    </p>   
+                </div>
+                <button className="btn btn-primary">Get in Touch</button>
+            </div>
+
+        </section>
+    )
 }
